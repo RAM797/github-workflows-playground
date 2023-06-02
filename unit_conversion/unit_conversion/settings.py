4f05b2338108fe7e5affab2918fb4f92c716592d
RAM797 <ram.koripalli@hpe.com>
2023-06-02 09:33:39 -0400

    making it better

--- unit_conversion/unit_conversion/settings.py
+++ unit_conversion/unit_conversion/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import django
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+django.setup()
 
 # Application definition
 
